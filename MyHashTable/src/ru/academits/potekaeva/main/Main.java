484429e18035fdfb2799cd6af2f3ba02edbf4cb0
KaterinaPotekaeva <shlynda@ngs.ru>
2017-12-27 10:49:07 +0600

    Hash

---
+++ MyHashTable/src/ru/academits/potekaeva/main/Main.java
@@ -0,0 +1,34 @@
+package ru.academits.potekaeva.main;
+
+import ru.academits.potekaeva.myHashTable.MyHashTable;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MyHashTable<Integer> hashTable = new MyHashTable<>();
+        hashTable.add(0);
+        hashTable.add(100);
+        hashTable.add(200);
+        hashTable.add(300);
+        hashTable.add(17);
+        hashTable.add(9);
+        MyHashTable<Integer> hashTable1 = new MyHashTable<>();
+        hashTable1.add(3);
+        hashTable1.addAll(hashTable);
+        System.out.println(hashTable);
+
+//        Iterator<Integer> iterator = hashTable.iterator();
+//
+//        iterator.hasNext();
+//        iterator.next();
+//        iterator.next();
+//        iterator.next();
+//        iterator.remove();
+//        iterator.next();
+//        iterator.next();
+//        iterator.next();
+//        System.out.println(hashTable);
+    }
+}
---
+++ MyHashTable/src/ru/academits/potekaeva/myHashTable/MyHashTable.java
@@ -0,0 +1,254 @@
+package ru.academits.potekaeva.myHashTable;
+
+import java.util.*;
+
+public class MyHashTable<T> implements Collection<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private ArrayList<T>[] list;
+    private int size;
+    private int modCount;
+
+    public MyHashTable() {
+        this(DEFAULT_CAPACITY);
+
+    }
+
+    public MyHashTable(int capacity) {
+        //noinspection unchecked
+        list = new ArrayList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            list[i] = new ArrayList<>();
+        }
+    }
+
+    private int getIndex(Object o) {
+        return Math.abs(Objects.hashCode(o) % list.length);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return contains(o, getIndex(o));
+    }
+
+    private boolean contains(Object o, int index) {
+        //noinspection SuspiciousMethodCalls
+        return list[index].contains(o);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyIterator();
+    }
+
+    private class MyIterator implements Iterator<T> {
+        private int currentIndex;
+        private int cursor;
+        private Iterator<T> iterator = list[0].iterator();
+        private int expectedModCount = modCount;
+        private T element;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        @Override
+        public T next() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+
+            if (!iterator.hasNext()) {
+                currentIndex++;
+            } else {
+                element = iterator.next();
+                cursor++;
+                return element;
+            }
+
+            for (int i = currentIndex; i < list.length; i++) {
+                iterator = list[i].iterator();
+                if (iterator.hasNext()) {
+                    element = iterator.next();
+                    cursor++;
+                    return element;
+                }
+                currentIndex++;
+            }
+            throw new NoSuchElementException();
+        }
+
+        @Override
+        public void remove() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+
+            if (element == null) {
+                throw new IllegalStateException();
+            }
+
+            iterator.remove();
+            expectedModCount++;
+            modCount++;
+            element = null;
+            cursor--;
+            size--;
+        }
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        int i = 0;
+        for (ArrayList<T> list : list) {
+            for (T t : list) {
+                array[i] = t;
+                i++;
+            }
+        }
+        return array;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        Objects.requireNonNull(a);
+
+        int aLength = a.length;
+        if (aLength < size) {
+            //noinspection unchecked
+            return (T1[]) Arrays.copyOf(toArray(), size, a.getClass());
+        }
+        //noinspection SuspiciousSystemArraycopy
+        System.arraycopy(toArray(), 0, a, 0, size);
+        if (aLength > size) {
+            a[size] = null;
+        }
+        return a;
+    }
+
+    @Override
+    public boolean add(T t) {
+        int index = getIndex(t);
+        return !contains(t, index) && add(t, index);
+    }
+
+    private boolean add(T t, int index) {
+        if (list[index].add(t)) {
+            modCount++;
+            size++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        Objects.requireNonNull(c);
+        int expectedModCount = modCount;
+
+        for (T listElement : c) {
+            add(listElement);
+        }
+        return expectedModCount != modCount;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return remove(o, getIndex(o));
+    }
+
+    private boolean remove(Object o, int index) {
+        //noinspection SuspiciousMethodCalls
+        if (list[index].remove(o)) {
+            modCount++;
+            size--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        Objects.requireNonNull(c);
+
+        for (Object e : c) {
+            if (!contains(e)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        Objects.requireNonNull(c);
+
+        int expectedModCount = modCount;
+        for (Object aC : c) {
+            remove(aC);
+        }
+        return expectedModCount != modCount;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        Objects.requireNonNull(c);
+
+        int expectedSize = size;
+        removeIf(next -> !c.contains(next));
+        return expectedSize != size;
+    }
+
+    @Override
+    public void clear() {
+        modCount++;
+
+        for (ArrayList<T> aList : list) {
+            aList.clear();
+        }
+        size = 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+
+        for (int i = 0; i < list.length; i++) {
+            stringBuilder.append("{");
+            int size = list[i].size();
+            int j = 0;
+
+            for (T t : list[i]) {
+                stringBuilder.append(t);
+                j++;
+                if (j < size) {
+                    stringBuilder.append(", ");
+                }
+            }
+
+            stringBuilder.append("}");
+            if (i == list.length - 1) {
+                stringBuilder.append("]");
+            } else {
+                stringBuilder.append(", ");
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
+
+
